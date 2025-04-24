c234fa1714d7a33d594bee180864888149edc8ca
zaindgr8 <66069515+zaindgr8@users.noreply.github.com>
2025-04-24 20:56:03 +0400

    Initial commit: Booking page with Cal.com integration

--- src/components/BookMeetingSection.tsx
+++ src/components/BookMeetingSection.tsx
@@ -8,13 +8,13 @@ const BookMeetingSection = () => {
     script.type = "text/javascript";
     script.innerHTML = `
       (function (C, A, L) { let p = function (a, ar) { a.q.push(ar); }; let d = C.document; C.Cal = C.Cal || function () { let cal = C.Cal; let ar = arguments; if (!cal.loaded) { cal.ns = {}; cal.q = cal.q || []; d.head.appendChild(d.createElement("script")).src = A; cal.loaded = true; } if (ar[0] === L) { const api = function () { p(api, arguments); }; const namespace = ar[1]; api.q = api.q || []; if(typeof namespace === "string"){cal.ns[namespace] = cal.ns[namespace] || api;p(cal.ns[namespace], ar);p(cal, ["initNamespace", namespace]);} else p(cal, ar); return;} p(cal, ar); }; })(window, "https://app.cal.com/embed/embed.js", "init");
-      Cal("init", "secret", {origin:"https://cal.com"});
-      Cal.ns.secret("inline", {
+      Cal("init", "30min", {origin:"https://cal.com"});
+      Cal.ns["30min"]("inline", {
         elementOrSelector:"#my-cal-inline",
         config: {"layout":"month_view"},
-        calLink: "devmate-solutions/secret",
+        calLink: "devmate-solutions/30min",
       });
-      Cal.ns.secret("ui", {"hideEventTypeDetails":false,"layout":"month_view"});
+      Cal.ns["30min"]("ui", {"hideEventTypeDetails":false,"layout":"month_view"});
     `;
     document.body.appendChild(script);
 
